099b0193c6e9943def48d2e27bb6859bb72721a0
h+ <serdar@justserdar.dev>
2025-06-21 18:15:55 +0200

    chore: New ServiceList component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -30,6 +30,7 @@ import Card from "./components/Card.vue";
 import CardGroup from "./components/CardGroup.vue";
 import Landing from "./layouts/Landing.vue";
 import Sections from "./components/Landing/Sections.vue";
+import ServicesList from "./components/Services/List.vue";
 import Features from "./components/Landing/Features.vue";
 import Installer from "./components/Landing/Installer.vue";
 import Referral from "./components/Landing/Referral.vue";
@@ -50,11 +51,12 @@ export default {
       spec,
     });
 
-    theme.enhanceApp({ app });
+    theme.enhanceApp({ app, router, siteData });
     app.component("Card", Card);
     app.component("CardGroup", CardGroup);
     app.component("LandingSection", Sections);
     app.component("LandingFeatures", Features);
+    app.component("ServicesList", ServicesList);
     app.component("Referral", Referral);
     app.component("Quickstart", Installer);
     app.component("Callout", Callout);
